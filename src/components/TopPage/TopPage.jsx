d425de9f9049eec704dbe1dc36e4f2f20a983145
voidrak <112881704+voidrak@users.noreply.github.com>
2023-12-22 11:53:12 +0300

    update

---
+++ src/components/TopPage/TopPage.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import Header from "../Header/Header";
+export const TopPage = () => {
+  return (
+    <div className="Top-Page">
+      <Header />
+    </div>
+  );
+};
