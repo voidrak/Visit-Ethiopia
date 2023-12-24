f826bab6e09df965d4a307bdd04a4ea124899e6a
voidrak <112881704+voidrak@users.noreply.github.com>
2023-12-24 21:34:20 +0300

    update

--- src/components/MidLanding/MidLanding.jsx
+++ src/components/MidLanding/MidLanding.jsx
@@ -5,7 +5,7 @@ export const MidLanding = () => {
     <div className="mid-landing">
       <div className="landing-text">
         <h2>DISCOVER</h2>
-        <h1>ETHIOPIA ETHIOPIA ETHIOPIA ETHIOPIA </h1>
+        <h1>ETHIOPIA </h1>
       </div>
       <img src="/LallibelaPic.png" alt="" />
     </div>
