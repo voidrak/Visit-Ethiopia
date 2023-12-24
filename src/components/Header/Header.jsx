7d33cb5300e74e3b3741eb0e70259702f0c57a26
voidrak <112881704+voidrak@users.noreply.github.com>
2023-12-24 10:57:29 +0300

    update

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,21 +3,25 @@ import { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Header = () => {
-  const [login, setLogin] = useState("Login");
-  const [signUp, setSignUp] = useState("Sign up");
+  const [topAuth, setTopAuth] = useState("Log in");
+  const [bottomAuth, setBottomAuth] = useState("Sign up");
 
   function handleAuthType(e) {
-    if (e.target.textContent === login) {
-      setLogin(signUp);
-    } else {
-      setSignUp(login);
+    if (e.target.textContent === "Sign up") {
+      setBottomAuth("Log in");
+      setTopAuth("Sign up");
+    } else if (e.target.textContent === "Log in") {
+      setBottomAuth("Sign up");
+      setTopAuth("Log in");
     }
   }
   return (
     <div className="header">
       <nav className="left-header">
         <ul>
-          <li className="yellow-bkg">MARA</li>
+          <li className="mara-logo">
+            <img src="/MARA.png" alt="" />
+          </li>
           <li>BOOK</li>
           <li>PACKAGES</li>
           <li>HOTELS</li>
@@ -32,13 +36,14 @@ const Header = () => {
           width="16"
           viewBox="0 0 512 512"
           fill="white"
+          className="search-svg"
         >
           <path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z" />
         </svg>
         <div className="drop-down">
           {/* use js logic to handle the drop down menu */}
           <button className="top-auth">
-            {login}{" "}
+            {topAuth}{" "}
             <svg
               xmlns="http://www.w3.org/2000/svg"
               height="16"
@@ -53,9 +58,9 @@ const Header = () => {
             <ul>
               <button
                 className="bottom-auth"
-                onCbuttonck={(e) => handleAuthType()}
+                onClick={(e) => handleAuthType(e)}
               >
-                {signUp}
+                {bottomAuth}
               </button>
             </ul>
           </div>
