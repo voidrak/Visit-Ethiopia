1813558ee7e80997fcf37f58a5c7849705567d1c
voidrak <112881704+voidrak@users.noreply.github.com>
2023-12-24 11:33:51 +0300

    update

---
+++ src/components/MidLanding/MidLanding.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const MidLanding = () => {
+  return (
+    <div className="mid-landing">
+      <div className="landing-text">
+        <h2>DISCOVER</h2>
+        <h1>ETHIOPIA ETHIOPIA ETHIOPIA ETHIOPIA </h1>
+      </div>
+      <img src="/LallibelaPic.png" alt="" />
+    </div>
+  );
+};
--- src/components/TopPage/TopPage.jsx
+++ src/components/TopPage/TopPage.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "../Header/Header";
+import { MidLanding } from "../MidLanding/MidLanding";
 export const TopPage = () => {
   return (
     <>
       <div className="Top-Page">
         <Header />
+        <MidLanding />
       </div>
     </>
   );
