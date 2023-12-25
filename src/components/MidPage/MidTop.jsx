ec2af805bd1044d715eb54d4514a8b4d56179916
voidrak <112881704+voidrak@users.noreply.github.com>
2023-12-25 11:02:42 +0300

    update

--- src/components/MidPage/MidTop.jsx
+++ src/components/MidPage/MidTop.jsx
@@ -3,12 +3,12 @@ import React from "react";
 export const MidTop = () => {
   return (
     <div className="mid-top">
-      <div className="right-mid">
+      <div className="left-mid">
         <h2 className="feature">FEATURE</h2>
         <h1>We make travel </h1>
         <h1>Easier</h1>
       </div>
-      <div className="left-mid">
+      <div className="right-mid">
         <p>
           We provide you with the best possible way to make it easier for you to
           travel to ethiopia and enjoy your tour
