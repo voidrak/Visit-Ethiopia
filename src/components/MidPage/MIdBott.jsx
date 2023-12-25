fae203ad1fb114d1dbddbe9cd486b83ee6c1c208
voidrak <112881704+voidrak@users.noreply.github.com>
2023-12-25 18:24:02 +0300

    update

---
+++ src/components/MidPage/MIdBott.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const MIdBott = () => {
+  return <div className="mid-bottom"></div>;
+};
--- src/components/MidPage/MidPage.jsx
+++ src/components/MidPage/MidPage.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { MidTop } from "./MidTop";
+import { MIdBott } from "./MIdBott";
 
 export const MidPage = () => {
   return (
     <div mid-page>
       <MidTop />
+      <MIdBott />
     </div>
   );
 };
